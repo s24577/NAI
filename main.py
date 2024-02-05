3004dbe55b10a7186cb70fc538a2c29cef899c2a
wiktokost <s24548@pjwstk.edu.pl>
2024-02-05 21:19:12 +0100

    small changes

--- main.py
+++
@@ -1,8 +0,0 @@
-from models.BERTbase import EmotionDetectionModel
-from models.facial_gesture import FacialGestureDetectionModel
-from models.pupil import PupilDetectionModel
-
-pupil = PupilDetectionModel()
-facial_gesture = FacialGestureDetectionModel()
-emotion = EmotionDetectionModel()
-

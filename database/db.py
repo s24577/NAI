0a5e2ef2083ffbd8851487a9bb543659e03388fc
kubaz <jakub.zawadzki16@gmail.com>
2024-02-06 23:55:07 +0100

    Added new dataset

--- database/db.py
+++ database/db.py
@@ -2,28 +2,35 @@ import csv
 
 from datasets import load_dataset
 
+def convert_label(label_example_input):
+    if label_example_input == 0:
+        return "negative"
+    elif label_example_input == 1:
+        return "positive"
+    elif label_example_input == 2 or label_example_input == 3:
+        return "neutral"
+
+
 # Załaduj zbiór danych
-dataset = load_dataset("papluca/language-identification")
+dataset = load_dataset("poem_sentiment")
 
-# Wybierz 100 przykładów z różnych języków
-languages_list = []
 sentences_list = []
+labels_list = []
 
-for language_example, sentence_example in zip(dataset['train']['labels'][:30], dataset['train']['text'][:30]):
-    # if language_example not in used_languages:
-    languages_list.append({"Language": language_example})
+for label_example, sentence_example in zip(dataset['train']['label'][:5], dataset['train']['verse_text'][:5]):
     sentences_list.append({"Text": sentence_example})
+    labels_list.append({"Label": convert_label(label_example)})
 
 with open("dataset.csv", "w", newline="", encoding="utf-8") as csvfile:
     # Utwórz obiekt writer
     csvwriter = csv.writer(csvfile)
 
     # Dodaj nagłówki do pliku CSV
-    csvwriter.writerow(["Predicted_language", "Sentence"])
+    csvwriter.writerow(["Predicted_sentiment", "Sentence"])
 
     # Przewiń przez przykłady
-    for language_example1, sentence_example1 in zip(languages_list, sentences_list):
+    for label_example1, sentence_example1 in zip(labels_list, sentences_list):
         # Zapisz do pliku CSV
-        csvwriter.writerow([language_example1["Language"], sentence_example1["Text"]])
+        csvwriter.writerow([label_example1["Label"], sentence_example1["Text"]])
 
 print("Wyniki zostały zapisane do pliku: dataset.csv")

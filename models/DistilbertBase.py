d4a090a62fc3134b86d8cb4f00d76e9ecf210b16
s24577 <s24577@pjwstk.edu.pl>
2024-02-07 18:00:44 +0100

    Comments

--- models/DistilbertBase.py
+++ models/DistilbertBase.py
@@ -1,27 +1,35 @@
+# Importowanie niezbędnych bibliotek
 from transformers import pipeline
 import pandas as pd
-
 from utils.calculator import calculate_metrics
 
+# Inicjalizacja klasyfikatora sentymentu z wykorzystaniem modelu
 distilled_student_sentiment_classifier = pipeline(
     model="lxyuan/distilbert-base-multilingual-cased-sentiments-student",
     top_k=None
 )
 
-
+# Definicja funkcji model_distilbert
 def model_distilbert(dataset_path):
+    # Wczytanie zestawu danych
     dataset = pd.read_csv(dataset_path)
+    # Inicjalizacja list do przechowywania rzeczywistych i przewidywanych sentymentów
     actual_sentiments, predicted_sentiments = [], []
 
+    # Iteracja przez każdy wiersz w zestawie danych
     for index, row in dataset.iterrows():
+        # Pobranie zdania i przewidywanego sentymentu z wiersza
         sentence = row["Sentence"]
         predicted_sentiment = row["Predicted_sentiment"]
-        # Zakładam, że 'actual_sentiment' jest prawdziwą etykietą, a nie przewidywaną
+        # Użycie klasyfikatora sentymentu do przewidzenia rzeczywistego sentymentu
         actual_sentiment = distilled_student_sentiment_classifier(sentence)[0][0]['label']
 
+        # Dodanie rzeczywistego i przewidywanego sentymentu do odpowiednich list
         actual_sentiments.append(actual_sentiment)
         predicted_sentiments.append(predicted_sentiment)
 
+    # Obliczenie metryk precyzji, pełności i F1
     precision, recall, f1 = calculate_metrics(actual_sentiments, predicted_sentiments)
 
+    # Zwrócenie metryk
     return precision, recall, f1
--- models/TwitterRobertBase.py
+++ models/TwitterRobertBase.py
@@ -1,26 +1,37 @@
+# Importowanie niezbędnych bibliotek
 import pandas as pd
 from transformers import pipeline, logging
 from utils.calculator import calculate_metrics
 
+# Ustawienie poziomu logowania na błąd
 logging.set_verbosity_error()
 
+# Inicjalizacja zadania analizy sentymentu z wykorzystaniem modelu i tokenizera
 sentiment_task = pipeline("sentiment-analysis",
                           model="cardiffnlp/twitter-roberta-base-sentiment-latest",
                           tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest")
 
-
+# Definicja funkcji model_twitter_robert
 def model_twitter_robert(dataset_path):
+    # Wczytanie zestawu danych
     dataset = pd.read_csv(dataset_path)
+    # Inicjalizacja list do przechowywania rzeczywistych i przewidywanych sentymentów
     actual_sentiments, predicted_sentiments = [], []
 
+    # Iteracja przez każdy wiersz w zestawie danych
     for index, row in dataset.iterrows():
+        # Pobranie zdania i przewidywanego sentymentu z wiersza
         sentence = row["Sentence"]
         predicted_sentiment = row["Predicted_sentiment"]
+        # Użycie zadania analizy sentymentu do przewidzenia rzeczywistego sentymentu
         actual_sentiment = sentiment_task(sentence)[0]['label']
 
+        # Dodanie rzeczywistego i przewidywanego sentymentu do odpowiednich list
         actual_sentiments.append(actual_sentiment)
         predicted_sentiments.append(predicted_sentiment)
 
+    # Obliczenie metryk precyzji, pełności i F1
     precision, recall, f1 = calculate_metrics(actual_sentiments, predicted_sentiments)
 
+    # Zwrócenie metryk
     return precision, recall, f1
